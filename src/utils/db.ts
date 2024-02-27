6b72fccc57027647ffb84e7369b3122e4c4ee93b
tuulikoo <tuuli.kivisaari@gmail.com>
2024-02-27 12:54:51 +0200

    yml

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config({path: __dirname + '/../.env'}); // Adjust the path based on your project structure
+dotenv.config({path: __dirname + '/../.env'}); 
 
 console.log('DB_URL:', process.env.DATABASE_URL);
 
 const mongoConnect = async () => {
   const connection = await mongoose.connect(process.env.DATABASE_URL as string);
-  console.log('DB connected successfully');
+  console.log('DB connected successfully**');
   return connection;
 };
 
